0ab4ee3a37679eea4fff353023f51b34790fdbba
Samuel Jeffman <sjeffman83@outlook.com>
2024-02-10 12:34:54 +0100

    Complete implementation of the class

--- Assignment2/TemperatureConverter.cs
+++ Assignment2/TemperatureConverter.cs
@@ -1,83 +1,175 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// < summary >
+/// Filename: TemperatureConverter.cs
+/// Created on: 2024-02-01 00:00:00
+/// Author: Samuel Jeffman
+/// </summary>
 
 namespace Assignment2
 {
+    /// <summary>
+    /// Class TemperatureConverter
+    /// </summary>
     public class TemperatureConverter
     {
+        /// <summary>
+        /// Convert Celsius value to Fahrenheit
+        /// </summary>
+        /// <param name="celsius"></param>
+        /// <returns></returns>
         private double ConvertCtoF(double celsius)
         {
-            return 9 / 5 * celsius + 32;
+            return 9.0 / 5.0 * celsius + 32;
         }
+
+        /// <summary>
+        /// Convert Fahrenheit value to Celsius
+        /// </summary>
+        /// <param name="fahrenheit"></param>
+        /// <returns></returns>
         private double ConvertFtoC(double fahrenheit)
         {
-            return (double)5 / 9 * (fahrenheit - 32);
+            return 5.0 / 9.0 * (fahrenheit - 32);
         }
 
+        /// <summary>
+        /// Iterate interval of Fahenheit values
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <param name="interval"></param>
         private void IterateFtoC(double limit, int interval)
         {
-            for (int i = 0; i <= limit; i += interval)
+            int fahrenheit = 0;
+            Console.WriteLine("\nConverting Fahrenheit to Celsius:\n");
+            do
             {
-                Console.WriteLine($"{i}F:{ConvertFtoC(i):f2}");
+                Console.WriteLine($"{fahrenheit}°F:{ConvertFtoC(fahrenheit):f2}°C");
+                fahrenheit += interval;
             }
-        }
-        private void IterateCtoF(double limit, int interval)
-        {
-            for (int i = 0; i <= limit; i += interval)
+            while (fahrenheit <= limit);
+
+            //Alternate solution
+            /*for (int fahrenheit = 0; fahrenheit <= limit; fahrenheit += interval)
             {
-                Console.WriteLine($"{i}F:{ConvertCtoF(i):f2}");
-            }
+                Console.WriteLine($"{fahrenheit}°F:{ConvertFtoC(fahrenheit):f2}°C");
+            }*/
         }
 
-        private void PresentData(int option)
+        /// <summary>
+        /// Iterate interval of Celsius values
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <param name="interval"></param>
+        private void IterateCtoF(double limit, int interval)
+        {
+            int celsius = 0;
+            Console.WriteLine("\nConverting Celsius to Fahrenheit:\n");
+            do
+            {
+                Console.WriteLine($"{celsius}°C:{ConvertCtoF(celsius):f2}°F");
+                celsius += interval;
+            }
+            while (celsius <= limit);
+
+            //Alternate solution
+            /*for (int celsius = 0; celsius <= limit; celsius += interval)
+            {
+                Console.WriteLine($"{celsius}°C:{ConvertCtoF(celsius):f2}°F");
+            }*/
+        }
+
+        /// <summary>
+        /// Present data based on input option
+        /// </summary>
+        /// <param name="option"></param>
+        private int PresentData(int option)
         {
             switch(option)
             {
-                case 0:
-                    Environment.Exit(0);
-                    break;
                 case 1:
                     IterateFtoC(210, 10);
+                    Pause();
                     break;
                 case 2:
                     IterateCtoF(100, 5);
+                    Pause();
                     break;
-                default:
+                case 0:
+                default: //Nothing to do.
                     break;
             }
-        }
 
-        private int ReadInput()
-        {
-            Console.WriteLine($"\nChoose one of the options below");
-            Console.WriteLine($"\n0. Exit application");
-            Console.WriteLine($"\n1. Convert from Fahrenheit to Celsius");
-            Console.WriteLine($"\n2. Convert from Celsius to Fahrenheit");
-            int option;
-            bool ok = int.TryParse(Console.ReadLine(), out option);
-            if(!ok)
-            {
-                Console.Clear();
-                ReadInput(); // This will cause recursion, fix!!!
-            }
-            else
-            {
-                if(option < 0 || option > 2)
-                {
-                    Console.Clear();
-                    ReadInput(); // This will cause recursion, fix!!!
-                }
-            }
             return option;
         }
 
+        /// <summary>
+        /// Read input as Integer and do checks for datatype and in range.
+        /// </summary>
+        /// <param name="rangeStart"></param>
+        /// <param name="rangeEnd"></param>
+        /// <returns></returns>
+        private int ReadInteger(int rangeStart, int rangeEnd)
+        {
+            int rangeCount = rangeEnd - rangeStart + 1;
+            int input;
+            bool isInt;
+            bool inRange = false;
+            do
+            {
+                string? str = Console.ReadLine();
+                isInt = int.TryParse(str, out input);
+                if (!isInt)
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+                else
+                {
+                    inRange = Enumerable.Range(rangeStart, rangeCount).Contains(input);
+                    if(!inRange)
+                    {
+                        Console.WriteLine($"Value out of range ({rangeStart}-{rangeEnd})!");
+                    }
+                }
+            }
+            while (!isInt || !inRange); //Continue until input is of datatype int and value is in range.
+            return input;
+        }
+
+        /// <summary>
+        /// Read input from console
+        /// </summary>
+        /// <returns></returns>
+        private int ReadInput()
+        {
+            Console.Clear();
+            Console.WriteLine($"\nChoose one of the options below\n");
+            Console.WriteLine($"1. Convert from Fahrenheit to Celsius");
+            Console.WriteLine($"2. Convert from Celsius to Fahrenheit");
+            Console.WriteLine($"0. Exit application\n");
+            int option = ReadInteger(0,2);
+            return option;
+        }
+
+        /// <summary>
+        /// Pause console and wait for input [ENTER]
+        /// </summary>
+        private void Pause()
+        {
+            Console.WriteLine("\nPress [ENTER] to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        /// <summary>
+        /// Execute methods of the class
+        /// </summary>
         public void Run()
-        { 
-            PresentData(ReadInput());
-            Run();
+        {
+            int option;
+            do
+            {
+                option = PresentData(ReadInput());
+            }
+            while (option != 0);
         }
     }
 }
